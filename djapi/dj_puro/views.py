e23bc7187a7ede58d1cc79dbe8a06f88cee179e4
Jorge Arevalo <jorge.arevalo@reclamador.es>
2019-09-20 14:41:05 +0200

    Added useful comments

--- djapi/dj_puro/views.py
+++ djapi/dj_puro/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, HttpResponse
 from .models import Category
 
+'''
+Sending JSON response from views is not something Django excels at. Check these links
+https://docs.djangoproject.com/en/2.2/topics/serialization/#serialization-formats-json
+https://docs.djangoproject.com/en/2.2/ref/request-response/#jsonresponse-objects
+https://docs.djangoproject.com/en/2.2/topics/class-based-views/mixins/#more-than-just-html
+'''
+
 
 def category_list(request):
     # We may need a filter...
@@ -22,9 +29,11 @@ def category_detail(request, pk):
     data = serialize('json', Category.objects.filter(pk=pk))
     return HttpResponse(data, content_type='application/json')
 
+
 def category_detail_2(request, pk):
     data = list(Category.objects.filter(pk=pk).values('active', 'name', 'description'))
     return JsonResponse(data[0], safe=False)
+
 
 def category_detail_3(request, pk):
     cat = get_object_or_404(Category, pk=pk)
