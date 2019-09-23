5a5eb0050f2f9fc9be0ed1303a59678b2c5bf63e
Jorge Arevalo <jorge.arevalo@reclamador.es>
2019-09-23 14:33:32 +0200

    Basic user management with token generation

--- djapi/api/models.py
+++ djapi/api/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 from .mixins import Timestampable, Publishable, Permalinkable, Activable, Showable
 
 # Create your models here.
@@ -45,3 +48,9 @@ class Product(Timestampable, Publishable, Permalinkable, Showable, Activable):
     class Meta:
         verbose_name = 'Producto'
         verbose_name_plural = 'Productos'
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
